a694e6df9b989b01525c14d08225b6af04e0bcd5
marinasupernova <marina.de.suvorova@gmail.com>
2021-03-09 18:07:46 +0100

    iq_test

--- 4 -iq-test/index.py
+++ 4 -iq-test/index.py
@@ -1,11 +1,29 @@
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
 def iq_test(numbers):
 
-    numbers = list(numbers)
+    numbers = list(map(lambda x: int(x) , numbers.split(" ")))
+    marker = False
+
+    if is_even(numbers[0])  == is_even(numbers[1]):
+        marker = is_even(numbers[0])
+    else:
+        marker = is_even(numbers[2])
+    
+    for i, num in enumerate(numbers):
+        if is_even(num) != marker:
+            return i+1
+ 
 
 
-    # for elem in range(numbers):
-    #     if elem % 2 == 1:
-            
+
+
+
         
 
 
