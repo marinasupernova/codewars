d868e4cb42d563aaf2748329199fc74d56e85799
marinasupernova <marina.de.suvorova@gmail.com>
2021-05-05 12:45:14 +0200

    24-mexican-wave

---
+++ 24-mexican-wave/index.py
@@ -0,0 +1,23 @@
+def wave(string):
+    mexican_wave = []
+    real_mexican_wave = []
+
+    for letter in string:
+        if letter != " ":
+            mexican_wave.append(list(string))
+
+    i = 0
+    length = len(mexican_wave)
+
+    while i < length:
+        mexican_wave[i][i] = mexican_wave[i][i].upper()
+        i += 1
+    
+    for letters in mexican_wave:
+      real_mexican_wave.append("".join(letters))
+
+    return real_mexican_wave
+
+# print(wave("hello"))
+print(wave("Two words"))
+    
