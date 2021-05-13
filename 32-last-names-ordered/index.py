f565232e69ca1c99639195ce594ed19249d9d2c9
marinasupernova <marina.de.suvorova@gmail.com>
2021-05-13 11:03:53 +0200

    32-checked

--- 32-last-names-ordered/index.py
+++ 32-last-names-ordered/index.py
@@ -1,5 +1,5 @@
 def sort_names(s):
-    sorted_names = ""
+    sorted_names = "" 
  
     persons = []
 
@@ -13,10 +13,12 @@ def sort_names(s):
         }
         persons.append("(" + split_name[1].upper() + ", " + split_name[0].upper() + ")")
 
-    sorted_names = ", ".join(sorted(persons))
+    sorted_names = "".join(sorted(persons))
 
     return sorted_names
 
 
 print(sort_names("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell")) 
 #print(sort_names("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern"))
+'(ARNO, ANN), (BELL, JOHN), (CORNWELL, ALEX), (DORNY, ABBA), (KERN, LEWIS), (KORN, ALEX), (META, GRACE), (SCHWARZ, VICTORIA), (STAN, MADISON), (STAN, MEGAN), (WAHL, ALEXIS)' should equal 
+'(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)'
